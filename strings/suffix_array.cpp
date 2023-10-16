b5f74f9b7940cd1610ef215689eabb95ec1bc880
Pietro Palombini <52302877+Aplietexe@users.noreply.github.com>
2023-10-16 13:48:39 -0300

    Update suffix_array.cpp

--- strings/suffix_array.cpp
+++ strings/suffix_array.cpp
@@ -1,26 +1,33 @@
-#define RB(x) (x<n?r[x]:0)
-void csort(vector<int>& sa, vector<int>& r, int k){
-	int n=sa.size();
-	vector<int> f(max(255,n),0),t(n);
-	fore(i,0,n)f[RB(i+k)]++;
-	int sum=0;
-	fore(i,0,max(255,n))f[i]=(sum+=f[i])-f[i];
-	fore(i,0,n)t[f[RB(sa[i]+k)]++]=sa[i];
-	sa=t;
+#define RB(x) (x < n ? r[x] : 0)
+void csort(vi &sa, vi &r, ll k)
+{
+    ll n = sa.size();
+    vi f(max(255ll, n), 0), t(n);
+    fori(i, 0, n) f[RB(i + k)]++;
+    ll sum = 0;
+    fori(i, 0, max(255ll, n)) f[i] = (sum += f[i]) - f[i];
+    fori(i, 0, n) t[f[RB(sa[i] + k)]++] = sa[i];
+    sa = t;
 }
-vector<int> constructSA(string& s){ // O(n logn)
-	int n=s.size(),rank;
-	vector<int> sa(n),r(n),t(n);
-	fore(i,0,n)sa[i]=i,r[i]=s[i];
-	for(int k=1;k<n;k*=2){
-		csort(sa,r,k);csort(sa,r,0);
-		t[sa[0]]=rank=0;
-		fore(i,1,n){
-			if(r[sa[i]]!=r[sa[i-1]]||RB(sa[i]+k)!=RB(sa[i-1]+k))rank++;
-			t[sa[i]]=rank;
-		}
-		r=t;
-		if(r[sa[n-1]]==n-1)break;
-	}
-	return sa;
+vi constructSA(string &s)
+{ // O(n logn)
+    ll n = s.size(), rank;
+    vi sa(n), r(n), t(n);
+    fori(i, 0, n) sa[i] = i, r[i] = s[i];
+    for (ll k = 1; k < n; k *= 2)
+    {
+        csort(sa, r, k);
+        csort(sa, r, 0);
+        t[sa[0]] = rank = 0;
+        fori(i, 1, n)
+        {
+            if (r[sa[i]] != r[sa[i - 1]] || RB(sa[i] + k) != RB(sa[i - 1] + k))
+                rank++;
+            t[sa[i]] = rank;
+        }
+        r = t;
+        if (r[sa[n - 1]] == n - 1)
+            break;
+    }
+    return sa;
 }
