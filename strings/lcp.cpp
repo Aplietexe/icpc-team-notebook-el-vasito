124f4343aa30c1a8721872fe6ea036aef9aa52b9
Pietro Palombini <52302877+Aplietexe@users.noreply.github.com>
2023-10-16 15:49:00 -0300

    Update lcp.cpp

--- strings/lcp.cpp
+++ strings/lcp.cpp
@@ -1,14 +1,21 @@
-vector<int> computeLCP(string& s, vector<int>& sa){
-	int n=s.size(),L=0;
-	vector<int> lcp(n),plcp(n),phi(n);
-	phi[sa[0]]=-1;
-	fore(i,1,n)phi[sa[i]]=sa[i-1];
-	fore(i,0,n){
-		if(phi[i]<0){plcp[i]=0;continue;}
-		while(s[i+L]==s[phi[i]+L])L++;
-		plcp[i]=L;
-		L=max(L-1,0);
-	}
-	fore(i,0,n)lcp[i]=plcp[sa[i]];
-	return lcp; // lcp[i]=LCP(sa[i-1],sa[i])
+vi computeLCP(string &s, vi &sa) // lcp[i]=LCP(sa[i-1],sa[i])
+{
+    ll n = SZ(s), L = 0;
+    vi lcp(n), plcp(n), phi(n);
+    phi[sa[0]] = -1;
+    fori(i, 1, n) phi[sa[i]] = sa[i - 1];
+    fori(i, 0, n)
+    {
+        if (phi[i] < 0)
+        {
+            plcp[i] = 0;
+            continue;
+        }
+        while (s[i + L] == s[phi[i] + L])
+            L++;
+        plcp[i] = L;
+        L = max(L - 1, 0ll);
+    }
+    fori(i, 0, n) lcp[i] = plcp[sa[i]];
+    return lcp;
 }
